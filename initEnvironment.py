10b7d0faed5eea67b5d041f677786f204976751f
Abdul Rehman <116005757+Abdul4237@users.noreply.github.com>
2022-10-17 19:39:27 +0500

    Update initEnvironment.py

--- initEnvironment.py
+++ initEnvironment.py
@@ -1,2 +1,24 @@
 class Player :
-    
+    def __init__(self):
+        self.deck=[]
+        self.stack=[]
+    def addToStack(cards):
+        self.stack.append(cards)
+    def removeFromStack(cards):
+        self.stack.pop()
+
+deck = {
+    2:4,
+    3:4,
+    4:4,
+    5:4,
+    6:4,
+    7:4,
+    8:4,
+    9:4,
+    10:4,
+    "J":4,
+    "Q":4,
+    "K":4,
+    "A":4
+}
